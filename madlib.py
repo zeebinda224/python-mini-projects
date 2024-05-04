551fbfd3beaec051cce434787988bdd01d899aea
zeebinda224 <124720743+zeebinda224@users.noreply.github.com>
2024-05-04 23:18:32 +0500

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,11 @@
+#string concatenation where f for format
+#youtuber="zee"
+#print(f"subscribe to {youtuber}")
+
+adj = input("Adjectie: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2}."
+
+print(madlib)
